e4564435abcd3fa549d14d048bd2ea2b281ae3e3
christoaluckal <christo12aluckal@gmail.com>
2021-11-22 16:02:06 +0530

    changed usage of folder

--- driver.py
+++ driver.py
@@ -27,31 +27,47 @@ def modify_image(original_folder,original_list,type):
     # pass
 
 
-def run_upscaler():
+def run_upscaler(split_output,upscaled_output):
     from subprocess import Popen
     os.chdir('Fast-SRGAN/')
     # Popen('cd Fast-SRGAN/',shell=True)
-    process = Popen('python3 infer.py --image_dir ../split_output/ --output_dir ../split_upscaled/',shell=True)
+    process = Popen('python3 infer.py --image_dir ../{}/ --output_dir ../{}/'.format(split_output,upscaled_output),shell=True)
     process.wait()
     os.chdir('../')
     return
 
+def create_temp_folders():
+    if not os.path.exists('split_output'):
+        os.mkdir('./split_output')
+    else:
+        clear_dir('split_output/')
+    if not os.path.exists('split_upscaled'):
+        os.mkdir('./split_upscaled')
+    else:
+        clear_dir('split_upscaled/')
+    if not os.path.exists('upscaled_big'):
+        os.mkdir('./upscaled_big')
+    else:
+        clear_dir('upscaled_big/')
+
+
 original_folder = args[0]
-output_folder = args[1]
+# output_folder = args[1]
 
 image_list = os.listdir(original_folder)
 # image_list = modify_image(original_folder,image_list,'resize')
+create_temp_folders()
 
 for x in image_list:
     start = time.time()
     # Split the larger image into smaller 96x96 images
-    vsteps,hsteps = split.split_96(original_folder+x,output_folder)
+    vsteps,hsteps = split.split_96(original_folder+x,'split_output/')
     # This function runs the inference code and generates the 384x384 upscaled image for the corresponding 96x96 image
-    run_upscaler()
+    run_upscaler('split_output','split_upscaled')
     # The upscaler scaled all 96x96 images into 384x384. Since they all belong to one original image, we stitch the 384x384 image to make the larger image
-    join.join_96(vsteps,hsteps,'/home/caluckal/Desktop/temp/split_upscaled','/home/caluckal/Desktop/temp/upscaled')
+    join.join_96(vsteps,hsteps,'split_upscaled','upscaled_big')
     # Delete the contents of the temp folder
-    clear_dir(output_folder)
+    clear_dir('split_output/')
     clear_dir('split_upscaled/')
     end = time.time()
     print("FINISHED:",x," in {}s".format(end-start))
