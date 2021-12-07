99bfa022cf3cef7eec0746426f807cc8eda7d574
christoaluckal <christo12aluckal@gmail.com>
2021-12-07 18:38:55 +0530

    sift

---
+++ validation/sift_match.py
@@ -0,0 +1,60 @@
+
+import os
+import cv2
+
+def match(img1,img2,name):
+#load images
+    # Initiate SIFT detector
+    img1 = cv2.imread(img1)
+    img2 = cv2.imread(img2)
+    sift = cv2.SIFT_create()
+
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(img1,None)
+    kp2, des2 = sift.detectAndCompute(img2,None)
+
+    # BFMatcher with default params
+    bf = cv2.BFMatcher(normType=cv2.NORM_L2, crossCheck=False)
+    try:
+        matches = bf.knnMatch(des1,des2, k=2)
+    except Exception:
+        return
+    # Apply ratio test
+    good = []
+    for m,n in matches:
+        if m.distance < 0.75*n.distance:
+            good.append([m])
+
+    img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=2)
+    cv2.imwrite(name+'.jpg',img3)
+    return good
+
+good_list = []
+
+og_folder = 'sift/og_parts'
+alt_folder = 'sift/alt_parts'
+
+
+og_list = sorted([x for x in os.listdir(og_folder) if x.endswith('.jpg')])
+alt_list = sorted([x for x in os.listdir(alt_folder) if x.endswith('.jpg')])
+
+match_list = []
+name_list = []
+for x in og_list:
+    names = x.split('_')
+    name = names[2]+'_'+names[3]
+    if 'alt_Ortho_'+name+'_.jpg' in alt_list:
+        match_list.append([x,'alt_Ortho_'+name+'_.jpg'])
+
+for x in match_list:
+    names = x[0].split('_')
+    name = names[2]+'_'+names[3]
+    good_list.append(match(og_folder+'/'+x[0],alt_folder+'/'+x[1],'sift/match/'+name))
+    name_list.append(name)
+
+match_file = open('sift/match_file.txt','w+')
+
+for x,y in zip(good_list,name_list):
+    if x is not None:
+        print(y,':',len(x))
+        match_file.write((str(y)+'.jpg  :'+str(len(x))+'\n'))
---
+++ validation/split.py
@@ -0,0 +1,131 @@
+import numpy as np
+import cv2
+
+image_map = {} # This data structure is used to store coordinates of each sub-part
+# The format of image_map is: for (ij)th part [(y1,x1),(y2,x2)] ie standard image format where (y1,x1) are top left and (y2,x2) are bottom right coordinates
+
+# Calculate the percent of white pixels in the image and reject if over threshold
+def sampleImage(img):
+    height,width,channels = img.shape
+    n_white_pix = np.sum(img == 0)
+    n_white_pix = n_white_pix //3
+    total_pixels = height*width
+    if(n_white_pix/total_pixels > 0.96):
+        return False
+    else:
+        return True
+
+# Since images need to be in a defined standard, we pad the image till it achieves the desired size. default_pad is the desired crop dimensions
+def padImage(img,default_pad):
+    height,width,channels = img.shape
+    # print(height,width)
+    aspect_ratio = float(width/height)
+    if aspect_ratio > 2.0 or aspect_ratio < 0.5:
+        print("Image is not close to standard aspect ratio\n")
+        exit()
+    else:
+        # Calculate the padding pixel number required for height and width
+        temp_width = width
+        width_pad = 0
+        temp_height = height
+        height_pad = 0
+        if width%default_pad[1] !=0:
+            # print("Padding width")
+            while(temp_width%default_pad[1]!=0):
+                temp_width+=1
+                width_pad+=1
+        if height%default_pad[0] !=0:
+            # print("Padding height")
+            while(temp_height%default_pad[0]!=0):
+                temp_height+=1
+                height_pad+=1
+
+        # print(height_pad,width_pad)
+        final_width = width+width_pad
+        final_height = height+height_pad
+        print("Original Image Resolution to :"+str(height)+"x"+str(width))
+        print("Image Padded to :"+str(final_height)+"x"+str(final_width))
+        color = (255,255,255)
+        result = np.full((final_height,final_width,channels), color, dtype=np.uint8)
+
+        # compute center offset
+        # xx = (final_width - width) // 2
+        # yy = (final_height - height) // 2
+
+        # copy img image into center of result image
+        # result[yy:yy+height, xx:xx+width] = img
+
+        # Bottom-Right Offset
+        result[:height,:width] = img
+        # save result
+        return result
+        
+# This function reads the image array, number of horizontal and vertical splits and the output directory
+def cropImage(img,h_split_num,v_split_num,op):
+    white = 0 # Total number of white pixels
+    sum_total = 0 # Total number of pixels
+    height,width,channel = img.shape
+    fin_img_list = [] # Final list that contains the names of the cropped images with a unique row-column identifying number
+    # print(height,v_split_num,width,h_split_num)
+    # print(height//v_split_num)
+    # print(width//h_split_num)
+    total = str((height//v_split_num)*(width//h_split_num))
+    print("Splitting image into "+total+" parts")
+    for i in range(0,height//v_split_num):
+        for j in range(0,width//h_split_num):
+            # print(i,j)
+            x_min,y_min = image_map["_"+str(i)+"_"+str(j)+"_"][0][1],image_map["_"+str(i)+"_"+str(j)+"_"][0][0]
+            x_max,y_max = image_map["_"+str(i)+"_"+str(j)+"_"][1][1],image_map["_"+str(i)+"_"+str(j)+"_"][1][0]
+            # print([i,j],image_map[(i,j)],x_min,y_min,x_max,y_min) #IMPORTANT  image_map stores into standard image format while XY min/max prints as human readable ie row first then column
+            temp_img = img[y_min:y_max,x_min:x_max]
+            # Sample the images and reject the mostly empty/white image parts
+            if sampleImage(temp_img):
+            # print("images/"+str(j)+str(i)+".jpg")
+                img_name = op+"_"+str(i)+"_"+str(j)+"_"+".jpg"
+                cv2.imwrite(img_name,temp_img)
+                fin_img_list.append(img_name)
+            else:
+                white+=1
+                # print("Popping",(i,j))
+                image_map.pop("_"+str(i)+"_"+str(j)+"_")
+            # print("End") 
+            sum_total+=1
+    print("Total white percentage:",str((white/sum_total)*100))
+    # print(image_map)
+    return fin_img_list
+
+# This function reads the padded image and the inputted default size to create an image map which maps the ij th element to the top left and bottom right coordinates
+def splitImage(img,default_size,op): # Notice the inversion of notations
+    height,width = img.shape[0],img.shape[1]
+    v_split_num = default_size[0] # The height of the cropped output
+    h_split_num = default_size[1] # The width of the cropped output
+    for i in range(0,height//v_split_num): # i is the row iterator ie ith row of a particular column
+        for j in range(0,width//h_split_num): # j is the column iterator ie jth row of a particular IMAGE
+            # image_map[j,i] = [[j*h_split_num,i*v_split_num],[(j+1)*h_split_num,(i+1)*v_split_num]]
+            image_map["_"+str(i)+"_"+str(j)+"_"] = [[i*v_split_num,j*h_split_num],[(i+1)*v_split_num,(j+1)*h_split_num]]
+            # print([i,j],image_map[i,j])
+        # print(image_map)
+    # print(image_map)
+    img_name = cropImage(img,h_split_num,v_split_num,op)
+    # sampleImage(img)
+    return img_name
+
+
+# Main driver function that takes the image path and output path and splits the image returning the split names, map and size  
+def breakImage(img_name,op):
+    img = cv2.imread(img_name)
+    print("Standard image height and width?")
+    default_size = int(input()),int(input())
+    if default_size[0] > img.shape[0] or default_size[1] > img.shape[1]:
+        print("Cannot break image into smaller parts. Inputted size is bigger than image width or height")
+        exit()
+    # We pad the image to the desired size so that the output is uniform
+    padded_img = padImage(img,default_size)
+    # Split the image and retain the coordinates of the split image as a row,col pair
+    img_name_list = splitImage(padded_img,default_size,op)
+    return img_name_list,image_map,default_size
+
+
+
+# breakImage('/home/caluckal/Desktop/Github/upscaler/validation/downscaled_upscaled_Ortho.png','sift/alt_Ortho')
+breakImage('/home/caluckal/Desktop/Github/upscaler/validation/original_Ortho.png','sift/og_parts/og_Ortho')
